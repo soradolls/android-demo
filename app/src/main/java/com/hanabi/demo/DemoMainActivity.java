6f0c4b34ed6c10e42ef717dfbfa1700c413c790e
soradolls <soradolls@gmail.com>
2016-09-03 17:57:09 +0800

    init project

---
+++ app/src/main/java/com/hanabi/demo/DemoMainActivity.java
@@ -0,0 +1,13 @@
+package com.hanabi.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DemoMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo_main);
+    }
+}
